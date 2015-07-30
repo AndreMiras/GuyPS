4968f34642b66687f52cbf491ae12abb257a5adc
Andre Miras <andre.miras@gmail.com>
2015-07-30 02:14:12 +0200

    status message widget

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 __version__ = '0.1'
 from kivy.garden.mapview import MapView, MapMarker, Coordinate
 from kivy.app import App
+from kivy.clock import Clock
+from kivy.uix.label import Label
 from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.screenmanager import Screen
 from kivy.properties import StringProperty, ObjectProperty
@@ -39,34 +42,63 @@ class CustomMapView(MapView):
         longitude = location.longitude
         self.center_on(latitude, longitude)
 
+class MapViewScreen(Screen):
+
+    status_message = StringProperty()
+    search_input_property = ObjectProperty()
+
+    def update_status_message(self, text, lifetime=3):
+        self.status_message = text
+        Clock.schedule_once(lambda dt: self.clean_status_message(), lifetime)
+
+    def clean_status_message(self):
+        self.update_status_message("")
+
 class Controller(RelativeLayout):
     mapview_screen_property = ObjectProperty()
+    mapview_property = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super(Controller, self).__init__(**kwargs)
+        self.mapview_property = self.mapview_screen_property.ids['mapview']
+        self.bind_events()
+
+    def bind_events(self):
+        search_input = self.mapview_screen_property.search_input_property
+        search_input.bind(on_text_validate=lambda instance: self.on_search(search_input.text))
 
     def gps_localize(self):
-        print "gps_localize"
-        # TODO: loading status
-        self.gps = gps
+        mapview_screen = self.mapview_screen_property
+        mapview_screen.update_status_message("Waiting for GPS location...", 10)
         try:
-            self.gps.configure(on_location=self.on_location, on_status=self.on_status)
-            self.gps.start()
+            gps.configure(on_location=self.on_location, on_status=self.on_status)
+            gps.start()
         except NotImplementedError:
+            message = "GPS not found."
+            mapview_screen.update_status_message(message)
             popup = PopupMessage(
                         title="Error",
-                        body="GPS not found.")
+                        body=message)
             popup.open()
 
     def on_location(self, **kwargs):
-        # TODO: close loading status
-        self.gps_location = '\n'.join(['{}={}'.format(k, v) for k, v in kwargs.items()])
-        # map_screen = self.manager.get_screen('map')
-        mapview = self.mapview_screen_property.ids['mapview']
+        gps.stop()
+        mapview = self.mapview_property
+        mapview_screen = self.mapview_screen_property
         latitude = kwargs['lat']
         longitude = kwargs['lon']
         mapview.center_on(latitude, longitude)
+        mapview_screen.update_status_message(
+            "Latitude: %s / Longitude: %s" % (round(latitude, 2), round(longitude, 2)), 10)
 
     def on_status(self, stype, status):
-        # TODO: use the status bar
-        self.gps_status = 'type={}\n{}'.format(stype, status)
+        mapview_screen = self.mapview_screen_property
+        mapview_screen.update_status_message(
+            'type={}\n{}'.format(stype, status), 10)
+
+    def on_search(self, text):
+        mapview_screen= self.mapview_screen_property
+        mapview_screen.update_status_message("Looking for \"%s\"" % (text))
 
 
 class MapViewApp(App):
