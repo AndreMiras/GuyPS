59c52bbd3d5d06c3ceaf7eef27b9bd295fefa035
Andre Miras <andre.miras@gmail.com>
2015-08-09 01:19:46 +0200

    awesomarkup helper for using Font Awesome

---
+++ awesomarkup.py
@@ -0,0 +1,14 @@
+FONT_PATH = 'static/fonts/fontawesome-4.3.0-webfont.ttf'
+
+class_unicode = {
+    'fa-map-marker': u'\uf041',
+    'fa-bars': u'\uf0c9',
+    'fa-map-pin': u'\uf276',
+    'fa-map-o': u'\uf278',
+    'fa-map': u'\uf279',
+}
+
+def awesomarkup(klass):
+    code = class_unicode[klass]
+    markup = u'[font=%s]%s[/font]' % (FONT_PATH, code)
+    return markup
