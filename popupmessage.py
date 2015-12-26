cda2e2abe8411bcf7893319d492e378af0615af9
Andre Miras <andre.miras@gmail.com>
2015-12-26 13:06:09 +0100

    popupmessage module docstring

--- popupmessage.py
+++ popupmessage.py
@@ -1,3 +1,8 @@
+"""
+Reusable PopupMessage class.
+Makes it easy to create simple popup messages with title and message body.
+The popup discarts when clicking on it or anywhere.
+"""
 from kivy.uix.popup import Popup
 from kivy.properties import StringProperty
 
