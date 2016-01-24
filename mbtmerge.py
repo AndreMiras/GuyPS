52f0276e3815dfaab45e2be7410c40a0e8d3746e
Andre Miras <andre.miras@gmail.com>
2016-01-24 20:49:46 +0100

    Improved mbtiles metadata merging
    Now handles minzoom and maxzoom merging.

--- mbtmerge.py
+++ mbtmerge.py
@@ -10,6 +10,55 @@ class MbtMerge(object):
     Handles mbtiles merging.
     """
 
+    def _merge_tiles_table(self, source, destination):
+        """
+        Merges tiles table from source to destination.
+        """
+        conn = sqlite3.connect(destination)
+        cursor = conn.cursor()
+        cursor.execute("ATTACH '%s' as db2" % (source))
+        cursor.execute("INSERT OR IGNORE INTO tiles SELECT * FROM db2.tiles")
+        conn.commit()
+        conn.close()
+
+    def _merge_metadata_zooms(self, source, destination):
+        """
+        Merges source and destination metadata minzoom and maxzoom values.
+        """
+        print "_merge_metadata_zooms"
+        conn = sqlite3.connect(destination)
+        cursor = conn.cursor()
+        cursor.execute("ATTACH '%s' as db2" % (source))
+        metadata = dict(cursor.execute("SELECT * FROM metadata"))
+        metadata2 = dict(cursor.execute("SELECT * FROM db2.metadata"))
+        min_zoom = int(metadata.get("minzoom", 12))
+        min_zoom2 = int(metadata2.get("minzoom", 12))
+        new_min_zoom = min(min_zoom, min_zoom2)
+        max_zoom = int(metadata.get("maxzoom", 14))
+        max_zoom2 = int(metadata2.get("maxzoom", 14))
+        new_max_zoom = max(max_zoom, max_zoom2)
+        cursor.execute("UPDATE metadata SET value=:value WHERE name='minzoom'", { "value": new_min_zoom })
+        cursor.execute("UPDATE metadata SET value=:value WHERE name='maxzoom'", { "value": new_max_zoom })
+        cursor.execute("DETACH db2")
+        conn.commit()
+        conn.close()
+
+    def _merge_metadata_bounds(self, source, destination):
+        """
+        Merges source and destination metadata bounds.
+        Creates the largest bounding box that contains both
+        source and destination.
+        """
+        # TODO
+        pass
+
+    def _merge_metadata_table(self, source, destination):
+        """
+        Merges source and destination metadata tables.
+        """
+        self._merge_metadata_zooms(source, destination)
+        self._merge_metadata_bounds(source, destination)
+
     def _merge_one(self, source, destination):
         """
         If destination is empty, creates it.
@@ -18,16 +67,8 @@ class MbtMerge(object):
         if not os.path.isfile(destination):
             copyfile(source, destination)
             return
-        conn = sqlite3.connect(destination)
-        cursor = conn.cursor()
-        # metadata = dict(cursor.execute("SELECT * FROM metadata"))
-        cursor.execute("ATTACH '%s' as db2" % (source))
-        cursor.execute("INSERT OR IGNORE INTO tiles SELECT * FROM db2.tiles")
-        # TODO: do a proper merging of metadata and merge zoom levels
-        cursor.execute("INSERT OR IGNORE INTO metadata SELECT * FROM db2.metadata")
-        cursor.execute("DETACH db2")
-        conn.commit()
-        conn.close()
+        self._merge_tiles_table(source, destination)
+        self._merge_metadata_table(source, destination)
 
     def merge(self, sources, destination):
         for source in sources:
